e8020ea6b001ea6f4bedf0267eb3a45af446780a
KarolSielski02 <mrkarlos2002@gmail.com>
2023-03-26 15:19:40 +0200

    Zjazd2 + praca domowa

---
+++ ProjectZaj02/src/test/java/pl/pjatk/ProjectZaj02/ProjectZaj02ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pjatk.ProjectZaj02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectZaj02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
